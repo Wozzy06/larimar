31ecbc52feca8ad942444f973704382ef78b67b2
Mead <maxime.crepieux@gmail.com>
2018-05-27 20:32:14 +0200

    :tada: first commit

--- spec/larimar_spec.cr
+++ spec/larimar_spec.cr
@@ -1,9 +1,39 @@
 require "./spec_helper"
 
 describe Larimar do
-  # TODO: Write tests
+  it "should add a well parsed line and fetch data in memory" do
+    Larimar.parse("test=hello")
+    Larimar.parse("test.azerty=world")
+    Larimar.parse("qwerty.mayonnaise=cornichon")
+    Larimar::DATA.size.should eq 3
+    Larimar.get("test").should eq "hello"
+    Larimar.get("test.azerty").should eq "world"
+    Larimar.get("qwerty.mayonnaise").should eq "cornichon"
+  end
 
-  it "works" do
-    false.should eq(true)
+  it "should reset data in memory" do
+    Larimar.flush
+    Larimar::DATA.size.should eq 0
+  end
+
+  it "should not do anything when the line is wrong or commented" do
+    Larimar.parse("dejdeldeev")
+    Larimar.parse("=1rdjnf")
+    Larimar.parse("#ceci.est.un.commentaire=this is a comment")
+    Larimar::DATA.size.should eq 0
+
+    Larimar.flush
+  end
+
+  it "should read and load a file and skip wrong/commented line" do
+    Larimar.load("./spec/test.properties")
+    Larimar::DATA.size.should eq 4
+    Larimar.flush
+  end
+
+  it "should raise an error when trying to fetch inexistant data (missing or mispelled)" do
+    expect_raises(Exception) do
+      Larimar.get("çanevapasmarcher")
+    end
   end
 end
--- src/larimar.cr
+++ src/larimar.cr
@@ -6,37 +6,18 @@ require "./larimar/version.cr"
 module Larimar
   extend self
 
-  private class CustomHash < Hash(String, CustomHash | String)
-  end
+  DATA = Hash(String, String).new
 
-  alias HashOrString = CustomHash | String
-
-  DATA = Hash(String, HashOrString).new
-
-  # get a loaded property 
+  # get a loaded property
   # raise an exception if the key is invalid
-  def get(key : String)
+  def get(key : String) : String
     keys = key.split('.')
-    data : String | HashOrString = data
-    i = 1
-    raise Exception.new("no property registered") if (keys.empty?)
-    data = key.fetch(keys[0])
-    while (key != keys.length)
-      temp = keys[i]
-      raise Exception.new("invalid key \"#{key}\"") unless (data.has_key?(temp))
-      data = data.fetch(temp)
-      if (keys.size != i)
-        raise Exception.new("invalid key \"#{key}\" : it should be the last") if (data.is_a?(String))
-      else
-        raise Exception.new("missing next key(s)") unless (data.is_a?(String))    
-      end
-      i += 1
-    end
-    return data.to_s
+    raise Exception.new("no property registered") unless (DATA.has_key?(key))
+    DATA[key]
   end
 
   # load properties from a file
-  def load(path : String)
+  def load(path : String) : Void
     lines = File.read_lines(path)
     lines.each do |line|
       parse(line)
@@ -45,38 +26,15 @@ module Larimar
 
   # parse one line (String) and register its content in memory
   # does nothing on invalid entry
-  def parse(line : String)
-    sides = line.split('=')
-    return if sides.size < 2 || sides[0] == "" || sides[1] == "" 
-    keys = sides[0].split('.')
-    if (DATA.has_key?(keys[0]))
-      data = DATA[keys[0]]
-    else
-      data = CustomHash.new  
-      DATA[keys[0]] = data
-    end
-    i = 1
-    while (i != keys.size)
-      if (data.has_key?(keys[i]))
-        data = data[keys[i]]
-      else
-        temp = CustomHash.new
-        data[key[i]] = temp
-        data = temp
-      end
-      i += 1
-    end
-    res = ""
-    i = 0
-    while (i != sides.size)
-      res = res.concat(sides[i])
-      i + =1
-    end
-    data[key[i]] = res
+  def parse(line : String) : Void
+    sides = line.split('=', 2)
+    return if (sides.size != 2 || sides[0] == "" || sides[1] == "" || sides[0].char_at(0) == '#')
+    key = sides[0]
+    DATA[key] = sides[1]
   end
 
   # delete all data
-  def flush
-    data.clear
+  def flush : Void
+    DATA.clear
   end
 end
